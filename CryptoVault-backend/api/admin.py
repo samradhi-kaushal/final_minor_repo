a95bd829323134656c5514ab16882b87ba7c20fe
Deepanshu Khurana <105312724+deepanshu-khurana@users.noreply.github.com>
2025-11-14 10:29:35 +0530

    Add 'receiving_user' to VaultFileAdmin list display

--- CryptoVault-backend/api/admin.py
+++ CryptoVault-backend/api/admin.py
@@ -4,5 +4,5 @@ from .models import VaultFile  # Import your correct model
 @admin.register(VaultFile)
 class VaultFileAdmin(admin.ModelAdmin):
     # Customize columns shown in the admin list view
-    list_display = ('id', 'uploaded_file', 'file_name', 'uploaded_at', 'blockchain_hash', 'user')
+    list_display = ('id', 'uploaded_file', 'file_name', 'uploaded_at', 'blockchain_hash', 'user', 'receiving_user')
     readonly_fields = ('uploaded_at',)
